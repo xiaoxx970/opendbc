56c1af21585ebf232a8ccc7c0a29ca0b94d73251
xiaoxx-mac <me@xiaoxx.cc>
2026-08-08 21:57:21 +0800

    VW MEB: restore override acceleration constant

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -125,6 +125,7 @@ class CarControllerParams:
 
       # Longitudinal constants
       self.ACCEL_INACTIVE = 3.01  # m/s^2
+      self.ACCEL_OVERRIDE = 0.00  # m/s^2
       self.JERK_LIMIT = 4.0  # m/s^3
       self.STARTING_ACCEL = 0.85  # m/s^2, minimum acceleration needed for a reliable brake release
       self.STARTING_VEGO = 0.5  # m/s, keep the start request active until the car is moving
